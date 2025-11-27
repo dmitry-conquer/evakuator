6fcd5418551cb13937fbd8d90d8c6f8bd6bcc86b
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-11-27 20:35:46 +0200

    Add AOS library and enhance footer and hero sections
    - Integrated AOS (Animate On Scroll) library for smooth animations.
    - Updated package.json and package-lock.json to include AOS and its types.
    - Revamped footer component with contact information and TikTok link.
    - Enhanced hero section with AOS attributes for improved visual effects.
    - Adjusted global styles and component-specific styles for better layout and responsiveness.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,9 +1,17 @@
 import "../styles/main.scss";
 
+import { BackTopButton } from "./components";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Scroll  from "./services/scroll";
+
 document.addEventListener("DOMContentLoaded", (): void => {
-  /**
-   * 
-   * Code here
-   * 
-   */
+  new BackTopButton(500, "home");
+
+  AOS.init({
+    duration: 600,
+    once: true,
+  });
+
+  new Scroll().initSmoothScroll();
 });
